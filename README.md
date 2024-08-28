3b9d30dd9a3ca76bf2dcc3b0a055aa160d60aecb
figueiredomarcelo <137330786+figueiredomarcelo@users.noreply.github.com>
2024-08-28 18:39:43 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Banner
+
+Play video with an advertising banner, and switch between the local and remote players.
+
+See the [Playing Video](https://developer.synamedia.com/senza/docs/playing-video) tutorial in the Senza developer documentation.
+
+## Build
+
+```bash
+npm ci
+npx webpack -w --config webpack.config.js
+open index.html
+```
---
+++ banner.js
@@ -0,0 +1,28 @@
+import { init, uiReady } from "senza-sdk";
+import { videoManager } from "./videoManager.js";
+import shaka from "shaka-player";
+
+const TEST_VIDEO = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+
+window.addEventListener("load", async () => {
+  try {
+    await init();
+    videoManager.init(new shaka.Player(video));
+    await videoManager.load(TEST_VIDEO);
+    videoManager.play();
+    uiReady();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+document.addEventListener("keydown", async function(event) {
+	switch (event.key) {
+    case "Enter": await videoManager.toggleBackground(); break;
+    case "Escape": videoManager.playPause(); break;
+    case "ArrowLeft": videoManager.skip(-30); break;
+    case "ArrowRight": videoManager.skip(30); break;      
+		default: return;
+	}
+	event.preventDefault();
+});
